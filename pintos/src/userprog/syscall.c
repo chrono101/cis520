f108a505a4701c4b04f27fb4dfbd09d265f7243e
hcarmitchel <heathercarmitchel@gmail.com>
2012-11-09 14:07:51 -0600

    Adding sys_handler

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -44,12 +44,46 @@ syscall_init (void)
 }
  
 /* System call handler. */
-
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+  printf ("System call!\n");
+  typedef int syscall_function (int, int, int);
+
+  /* A system call. */
+  struct syscall
+  {
+  size_t arg_cnt; /* Number of arguments. */
+  syscall_function *func; /* Implementation. */
+  };
+
+  /* Table of system calls. */
+  static const struct syscall syscall_table[] =
+  {
+ 	 {0, (syscall_function *) sys_halt},
+  	 {1, (syscall_function *) sys_exit},
+	 {1, (syscall_function *) sys_exec},
+	 {1, (syscall_function *) sys_wait},
+	 {2, (syscall_function *) sys_create},
+	 {1, (syscall_function *) sys_remove},
+	 {1, (syscall_function *) sys_open},
+  }
+  const struct syscall *sc;
+  unsigned call_nr;
+  int args[3];
+  /* Get the system call. */
+  copy_in (&call_nr, f->esp, sizeof call_nr);
+  if (call_nr >= sizeof syscall_table / sizeof *syscall_table)
   thread_exit ();
+  sc = syscall_table + call_nr;
+ 
+  /* Get the system call arguments. */
+  ASSERT (sc->arg_cnt <= sizeof args / sizeof *args);
+  memset (args, 0, sizeof args);
+  copy_in (args, (uint32_t *) f->esp + 1, sizeof *args * sc->arg_cnt);
+
+  /* Execute the system call, and set the return value. */
+  f->eax = sc->func (args[0], args[1], args[2]);
 }
 
 /* Returns true if UADDR is a valid, mapped user address,
